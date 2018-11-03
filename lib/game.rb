dcfe9a5521a2e6159c630169503c169a004cb2f0
kvdoherty <kvdoherty@gmail.com>
2018-11-03 17:06:49 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -58,7 +58,7 @@ def won?
     end
     
     if @winningcomboforxcount == 3 || @winningcomboforocount == 3
-      @winningcombo = [possiblewiningcombo]
+      @winningcombo << possiblewiningcombo
       @winningcombo
     end
   end
